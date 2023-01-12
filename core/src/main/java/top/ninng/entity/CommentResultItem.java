ab099ab8287db53a7a360e686cc2d8ca4d894e43
OhmLaw <xx1599xx@163.com>
2023-01-12 14:20:19 +0800

    feat(CommentModule): 增加评论实体和持久层

---
+++ core/src/main/java/top/ninng/entity/CommentResultItem.java
@@ -0,0 +1,46 @@
+package top.ninng.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * @Author OhmLaw
+ * @Date 2023/1/11 19:21
+ * @Version 1.0
+ */
+public class CommentResultItem implements Serializable {
+
+    private static final long serialVersionUID = 6917470717255079138L;
+
+    Comment comment;
+    ArrayList<Comment> childCommentList;
+
+    public CommentResultItem(Comment comment, ArrayList<Comment> childCommentList) {
+        this.comment = comment;
+        this.childCommentList = childCommentList;
+    }
+
+    public ArrayList<Comment> getChildCommentList() {
+        return childCommentList;
+    }
+
+    public void setChildCommentList(ArrayList<Comment> childCommentList) {
+        this.childCommentList = childCommentList;
+    }
+
+    public Comment getComment() {
+        return comment;
+    }
+
+    public void setComment(Comment comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public String toString() {
+        return "CommentResultItem{" +
+                "comment=" + comment +
+                ", childComment=" + childCommentList +
+                '}';
+    }
+}
